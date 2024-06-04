0d13a12487381aac4d7475fece9cae991f506434
XungHi <tjddnjs612@gmail.com>
2024-06-04 22:21:38 +0900

    Player + Player1 Move&anime
    플레이어 + 플레이어1 : 움직임,애니메이션

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     // 행동 함수
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    
+    //구시대 input 사용 -> input package 사용!
     void Update()
     {
         inputVec.x = Input.GetAxisRaw("Horizontal");
         inputVec.y = Input.GetAxisRaw("Vertical");
     }
+
+    //신시대 input package 내장함수 -> 이거 나중에 써야햠!
+    /*void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }*/
 
     void FixedUpdate()
     {
@@ -34,4 +46,16 @@ public class Player : MonoBehaviour
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; //대각선 같은 속도 
         rigid.MovePosition(rigid.position + nextVec); 
     }
+
+    //후처리 함수
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+
 }
